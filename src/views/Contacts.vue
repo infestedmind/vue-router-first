cda5665063b9671e68e26a86a20941f78d2019b9
yes <myinfestedmind@gmail.com>
2020-01-11 14:23:13 +0300

    done

---
+++ src/views/Contacts.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="home">
+    <h1>Здесь будут наши контакты.</h1>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="home">
+    <h1>Добропожаловать на наш проект!</h1>
+  </div>
+</template>
